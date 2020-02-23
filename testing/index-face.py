54cef407e2fb0506d69054f9fb0ce5e107d97be4
Harsha Vardhan Reddy <koppelaharsha@gmail.com>
2020-02-24 00:43:34 +0530

    adjusted image path

--- testing/index-face.py
+++ testing/index-face.py
@@ -2,5 +2,5 @@ from ..aws.rekognition.faces import index_face
 
 if __name__ == '__main__':
     collection_id = 'My-Collection'
-    image_path = '../../images/img-7.jpg'
+    image_path = '../images/img-7.jpg'
     index_face(collection_id, image_path)
--- testing/search_face.py
+++ testing/search_face.py
@@ -1,7 +1,6 @@
 from ..aws.rekognition.faces import search_face
 
 if __name__ == '__main__':
-    image_path = '../../images/img-8.jpg'
-    collectionId = 'My-Collection'
-    search_face(image_path, collectionId)
-    
+    collection_id = 'My-Collection'
+    image_path = '../images/img-8.jpg'
+    search_face(collection_id, image_path)
